35e05fa13b407aebecffecfb32742aa7f9fd1c7e
Lyubomir Ganchev <luboganchev91@gmail.com>
2014-10-07 11:18:30 -0700

    Refactor app.js

--- SchoolSystem/app.js
+++ SchoolSystem/app.js
@@ -1,38 +1,14 @@
 ﻿var express = require('express');
-var routes = require('./server/config/routes');
-var path = require('path');
 
-var logger = require('morgan');
-var session = require('express-session');
-var bodyParser = require('body-parser');
-var multer = require('multer');
+var env = process.env.NODE_ENV || 'development';
 
 var app = express();
+var config = require('./server/config/config')[env];
 
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'public/views'));
-app.set('view engine', 'jade');
+require('./server/config/express')(app, config);
+require('./server/config/mongoose')(config);
+require('./server/config/passport')();
+require('./server/config/routes')(app);
 
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(session({
-    resave: true,
-    saveUninitialized: true,
-    secret: 'uwotm8'
-}));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(multer());
-app.use(express.static(path.join(__dirname, 'public')));
-
-//// development only
-//if ('development' == app.get('env')) {
-//    app.use(errorHandler());
-//}
-
-app.get('/', routes.index);
-
-app.listen(app.get('port'), function () {
-    console.log('Express server listening on port ' + app.get('port'));
-});
+app.listen(config.port);
+console.log("Server running on port: " + config.port);
