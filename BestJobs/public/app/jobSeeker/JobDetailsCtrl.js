be1575de9955696e152d48536392bab76e9337f7
Lyubomir Ganchev <luboganchev91@gmail.com>
2014-10-09 08:09:52 -0700

    apply for a job

--- BestJobs/public/app/jobSeeker/JobDetailsCtrl.js
+++ BestJobs/public/app/jobSeeker/JobDetailsCtrl.js
@@ -1,7 +1,18 @@
 ﻿'use strict'
 
-app.controller('JobDetailsCtrl', function ($scope, $location, $http, $routeParams) {
-    $http.get('/api/jobs/'+ $routeParams.id).then(function (res) {
+app.controller('JobDetailsCtrl', function ($scope, $location, $http, $routeParams, notifier) {
+    
+    var currentId = $routeParams.id;
+
+    $http.get('/api/jobs/'+ currentId).then(function (res) {
         $scope.job = res.data;
     });
+
+    $scope.apply = function () {
+        $http.put('/api/jobs/' + currentId).then(function (res) { 
+            notifier.success(res.data);
+        }, function (err) { 
+            notifier.error(err.data);
+        });
+    }
 });
